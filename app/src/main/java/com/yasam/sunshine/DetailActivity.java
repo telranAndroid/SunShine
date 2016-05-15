bbe5e9fea2cb2826783054d853b7ab9727bf8469
telranAndroid <telran.asana@gmail.com>
2016-05-15 03:42:15 +0300

    046_03_Display_Forecast_Info_in_DetailActivity

--- app/src/main/java/com/yasam/sunshine/DetailActivity.java
+++ app/src/main/java/com/yasam/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.yasam.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent callerIntent = getIntent();
+        if(callerIntent!=null && callerIntent.hasExtra(Intent.EXTRA_TEXT)){
+            String forecast = callerIntent.getStringExtra(Intent.EXTRA_TEXT);
+
+            TextView txtVw_detail = (TextView) findViewById(R.id.txtVw_detail);
+
+            if(txtVw_detail!=null)
+                txtVw_detail.setText(forecast);
+        }
     }
 }
